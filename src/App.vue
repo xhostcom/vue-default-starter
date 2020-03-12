5537ec004bdc690874e867b3bc10f46c4b118b83
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-12 17:47:28 +0000

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ status }}</h1>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      status: "Hello World"
+    }
+  },
+  created: function () {
+
+  },
+  methods: {
+
+  },
+  components: {}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+ margin: 0;
+ padding: 0;
+ box-sizing:border-box;
 }
 </style>
