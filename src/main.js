a96df29f1d66b698ff1440dcddb43f4e76cd4c7e
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-22 10:01:34 +0000

    Update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "./index.js";
+import axios from "axios";
+
+Vue.prototype.$http = axios;
+Vue.prototype.axios = axios;
 
 Vue.config.productionTip = false
 
 new Vue({
+axios,
   render: h => h(App),
 }).$mount('#app')
